319663010be43cf683f71d2e70eb8e55e8434c5a
shizukuwu365 <juandita365@gmail.com>
2025-07-11 10:46:31 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.showString("go")
+basic.showNumber(1)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . . # . .
+    . . . . .
+    `)
+basic.showNumber(1)
+basic.showNumber(1 + 1)
+basic.showNumber(1)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . # # # .
+    . . . . .
+    . . . . .
+    `)
+basic.showNumber(1)
+basic.showNumber(2 - 2)
